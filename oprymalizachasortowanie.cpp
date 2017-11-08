21c00702f76048addcc533adfb4cd7718cccab18
zinker1997 <32900782+zinker1997@users.noreply.github.com>
2017-11-08 14:34:06 +0100

    optymalizacja.cpp

---
+++ oprymalizachasortowanie.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+using namespace std;
+
+int maxmin()
+{
+	srand(time(0));
+	int const N=10;
+	int tab[N];
+	for(int i=0;i<N;i++)
+		{
+		tab[i]=rand()%100;
+		cout<<tab[i]<<" ";
+		}
+	int k=tab[1];
+	int l=tab[1];
+	for(int i=0;i<N;i++)
+		{
+		if(tab[i]>=k)
+			{
+			k=tab[i];
+			}
+		}
+    cout<<endl;
+	cout<<k;
+	for(int i=0;i<N;i++)
+		{
+		if(tab[i]<=l)
+			{
+			l=tab[i];
+			}
+		}
+	cout<<endl;
+	cout<<l;
+	return 0;
+}
+int sortowanie()
+{
+	
+	srand(time(0));
+	int const N=10;
+	int tab[N];
+	bool zmienna=true;
+	for(int i=0;i<N;i++)
+		{
+		tab[i]=rand()%100;
+		
+		}
+	
+	while(zmienna)
+	{
+		zmienna=false;//zmiana z flaga
+		
+	for(int i=0;i<N;i++)
+	{
+		
+		if(tab[i]>tab[i+1])
+		{
+			
+			int k=tab[i];
+			tab[i]=tab[i+1];
+			tab[i+1]=k;
+			zmienna=true;
+			
+			
+		}
+		
+		
+		
+	}
+	
+	}	
+	cout<<endl;
+		for(int i=0;i<N;i++)
+	{
+		cout<<tab[i]<<" ";
+		
+	}
+	return 0;
+}
+
+
+int main()
+{
+maxmin();
+cout<<endl;
+sortowanie();
+return 0;
+}
